7a7e90ffd9246a25025c8a8da30be4822555b0e2
Simon Moushabeck <SimonMoush@gmail.com>
2017-10-09 16:08:52 -0500

    create model and view. Model doesn't compile because of ownership laws

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
 extern crate piston_window;
 
-use piston_window::*;
+mod model;
+mod view;
 
+use view::*;
+use piston_window::*;
 
 fn main() {
     // make the window
     let mut window: PistonWindow = WindowSettings::new("hey hey!", [500; 2])
-        .build().unwrap();
+        .exit_on_esc(true).build().unwrap();
 
     //event loop
     while let Some(e) = window.next(){
-        render(&mut window, e);
+        render(&mut window, &e);
     }
-}
-
-fn render(window: &mut PistonWindow, e: &Event) {
-        window.draw_2d(&e, |c, g| {
-            clear([1.0; 4], g);
-            rectangle([1.0, 1.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0], // position and dimensions
-                      c.transform, g);
-        });
 }
