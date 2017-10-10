f9cea433afe678a87e2851826d355bc258603ba2
Simon Moushabeck <SimonMoush@gmail.com>
2017-10-10 17:55:48 -0500

    only render and update on render and update events

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,23 @@ use piston_window::*;
 
 fn main() {
     // make the window
-    let mut window: PistonWindow = WindowSettings::new("hey hey!", [500; 2])
+    let mut window: PistonWindow = WindowSettings::new("waves", [500; 2])
         .exit_on_esc(true).build().unwrap();
 
     // make the model
-    let mut m = PressureField::with_dot(100, 100);
+    let mut m = PressureField::with_dot(50, 50);
 
     //event loop
     while let Some(e) = window.next(){
-        m.update();
-        render(&mut window, &e, &m);
+        match e {
+            Event::Loop(l) => {
+                match l {
+                    Loop::Render(r) => render(&mut window, &e, &m),
+                    Loop::Update(u) => m.update(),
+                    _ => (),
+                }
+            },
+            _ => (),
+        }
     }
 }
